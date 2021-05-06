1c1616097ccd2e0fe05068c34f88781aac2e5a2f
SYamada <yamada_shota@lightcafe.co.jp>
2021-05-06 18:47:41 +0900

    Add email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :post => '587',
+  :authentication => :plain,
+  :user_name => 	"",
+  :password => "",
+  :enable_starttls_auto => true
+}
